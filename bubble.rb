46e328b95bedd43039cd69e94288a3f02024ac35
orangesaft <156236994+orangesaft@users.noreply.github.com>
2024-11-13 12:48:16 +0900

    finished

--- bubble.rb
+++ bubble.rb
@@ -4,7 +4,24 @@ puts "Enter values (enter one value and hit enter. repeat until you are done wit
 loop do
   number = gets.chomp
   break if number == "done"
-  values << number
+  values << number.to_i
 end
 
-p values
+#p values
+
+def bubble_sort(values)
+  n = values.length
+  loop do
+    swapped = false
+    (n-1).times do |i|
+      if values[i] > values[i +1]
+        values[i], values[i+1] = values[i+1], values[i]
+        swapped = true
+      end
+    end
+    break unless swapped
+  end
+  values
+end
+
+p bubble_sort(values)
